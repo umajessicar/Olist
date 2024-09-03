95d2ea422d290cd7b0850597f4a90756a9d222ba
Jessica Rocha <rocha.jessica.p@gmail.com>
2024-09-03 12:35:59 +0000

    alterações gold (fernanda)

--- models/Gold/freight_sales.sql
+++ models/Gold/freight_sales.sql
@@ -4,39 +4,42 @@ WITH average_freight AS (
     FROM 
         {{ ref('silver_table') }}
 ),
-freight_ranges AS (
-    SELECT 
-        s.product_id,
-        s.product_category_name,
+sales_analysis AS (
+    SELECT
+        product_id,
+        product_category_name,
+        price,
+        freight_value,
         CASE
-            WHEN s.freight_value <= af.avg_freight_value THEN 'Low'
+            WHEN price <= 100 THEN 'Low'
+            WHEN price BETWEEN 100 AND 500 THEN 'Medium'
             ELSE 'High'
-        END AS freight_range,
-        s.freight_value
+        END AS price_range,
+        CASE
+            WHEN freight_value <= af.avg_freight_value THEN 'Low'
+            ELSE 'High'
+        END AS freight_range
     FROM 
         {{ ref('silver_table') }} s
     CROSS JOIN 
         average_freight af
-),
-sales_analysis AS (
-    SELECT
-        fr.freight_range,
-        SUM(s.price) AS total_sales,
-        COUNT(DISTINCT s.product_id) AS total_products
-    FROM 
-        {{ ref('silver_table') }} s
-    JOIN 
-        freight_ranges fr ON s.product_id = fr.product_id
-    GROUP BY 
-        fr.freight_range
 )
 SELECT
+    price_range,
     freight_range,
-    total_sales,
-    total_products,
-    total_sales / NULLIF(total_products, 0) AS avg_sales_per_product
+    SUM(price) AS total_sales,
+    COUNT(DISTINCT product_id) AS total_products,
+    AVG(price) AS avg_price_per_product,
+    AVG(freight_value) AS avg_freight_value,
+    SUM(price) / NULLIF(COUNT(DISTINCT product_id), 0) AS avg_sales_per_product
 FROM 
     sales_analysis
+GROUP BY 
+    price_range,
+    freight_range
 ORDER BY 
+    price_range,
+    freight_range,
     total_sales DESC
+
 
